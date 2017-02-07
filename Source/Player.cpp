320a603c897cf7bf0cc600b92ecce935b96a3887
Charlie Grasse <cgrasse@gmail.com>
2017-02-07 12:25:42 -0500

    Add files via upload

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -9,34 +9,31 @@
 
 void PlayerMove()
 {
-	for ( int i = 10; i < 30; i++ )
-	{
-		
+	
+		int i = 10;
 		if (dbKeyState(DIK_D) && dbSpriteX(i) < 608)
 		{
 			dbRotateSprite(i,90);
 			dbMoveSprite(i,1);
-			dbPlaySprite(i,1,8,60);
+			dbPlaySprite(i,1,6,60);
 		}
 		else if (dbKeyState(DIK_W) && dbSpriteY(i) > 32)
 		{
 			dbRotateSprite(i,0);
 			dbMoveSprite(i,1);
-			dbPlaySprite(i,1,8,60);
+			dbPlaySprite(i,1,6,60);
 		}
 		else if (dbKeyState(DIK_S) && dbSpriteY(i) < 608)
 		{
 			dbRotateSprite(i,180);
 			dbMoveSprite(i,1);
-			dbPlaySprite(i,1,8,60);
+			dbPlaySprite(i,1,6,60);
 		}
 		else if (dbKeyState(DIK_A) && dbSpriteX(i) > 32)
 		{
 			dbRotateSprite(i,-90);
 			dbMoveSprite(i,1);
-			dbPlaySprite(i,1,8,60);
+			dbPlaySprite(i,1,6,60);
 		}
 		CollisionDetection(i);
-	}
 }
-
