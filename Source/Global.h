5c9a1998d51afe444d43a8a3824086337668a1e5
Charlie Grasse <cgrasse@gmail.com>
2017-02-07 21:07:57 -0500

    Update Global.h
    adding comments

--- Source/Global.h
+++ Source/Global.h
@@ -1,11 +1,15 @@
 #pragma once
 
-
+/*******************
+**** Definitions ***
+*******************/
 #define MAP_HEIGHT 11
 #define MAP_WIDTH  10
 #define moveSpeed 4
+// Sprite iID for animation, probably can move to player.h
 #define iAnt 10
 
+// level map array
 const char mapArray[MAP_WIDTH][MAP_HEIGHT] ={	"WWWWWWWWWX",
 												"WFFFFWFWFF",
 												"WFWWFFFWFW",
