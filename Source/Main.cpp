a977aaf8eba004fca7f60d4ccd26e71b48eb30e1
Charlie Grasse <cgrasse@gmail.com>
2017-02-07 21:14:14 -0500

    Update Main.cpp
    added comments

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -3,44 +3,40 @@
 #include "DarkGDK.h"
 #include "mapping.h"
 #include "Player.h"
-											// the main entry point for the application is this function
+// the main entry point for the application is this function
 void DarkGDK ( void )
-{		
+{
+	// Probably move this to it's own function in an attempt to keep the main small
 	dbSyncOn   ( );
 	dbSyncRate (60);
 	dbSetDisplayMode(1024,768,32);
-	
 	dbSetWindowPosition(46,46);
-		
 	dbDisableEscapeKey ( );
-
-	int placeHolder = 0; //DarkGDK needs a variable in here for some reason!
-	//dbRandomize ( dbTimer ( ) );
-
 	
+	// Don't know why it won't run with out a declared variable in main!
+	int placeHolder = 0; //DarkGDK needs a variable in here for some reason!
+	
+	
+	//dbRandomize ( dbTimer ( ) ); --I don't know if I'll need a randomizer yet
+
+	// these declarations will be moved to mapping
 	dbLoadImage ( "Floor.bmp", 1);
 	dbLoadImage ( "Wall.bmp", 2);
-	DrawMap();
 	
+	// DrawMap will move to game function
+	DrawMap();	
 	
-
-	
+	// this will move game function
 	dbSetImageColorKey ( 255, 0, 255 );
-
-	
-	
 	CreateAntSprite();
 
 	// now we come to our main loop, we call LoopGDK so some internal
 	// work can be carried out by the GDK
 	while ( LoopGDK ( ) )
 	{
-		// run a loop through all our sprites
 		
-			// move the sprite down and play its animation
-			// moving from frame 1 to 16 with a delay of 60 ms
+		PlayerMove();	
 		
-		PlayerMove();		
 		// here we check if the escape key has been pressed, when it has
 		// we will break out of the loop
 		if ( dbEscapeKey ( ) )
@@ -54,8 +50,7 @@ void DarkGDK ( void )
 	// and we can free up any previously allocated resources
 	
 	// delete all the sprites
-	
-	//	dbDeleteSprite ( iAnt );
+	dbDeleteSprite ( iAnt );
 	// delete the backdrop image
 	dbDeleteImage ( 1 );
 	dbDeleteImage ( 2 );
