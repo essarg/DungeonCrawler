17747162a27ab4b79661ccad73e65753aab12a95
Charlie Grasse <cgrasse@gmail.com>
2017-02-13 09:34:58 -0500

    Scrolling Map
    Buggy scrolling map

--- Source/Global.h
+++ Source/Global.h
@@ -3,20 +3,60 @@
 /*******************
 **** Definitions ***
 *******************/
-#define MAP_HEIGHT 11
-#define MAP_WIDTH  10
-#define moveSpeed 4
-// Sprite iID for animation, probably can move to player.h
-#define iAnt 10
+
+#define DIRECTINPUT_VERSION 0x0800
+// Game map height and width
+#define MAP_WIDTH  21
+#define MAP_HEIGHT 20
+
+
+
+/******************
+**** Constants ****
+******************/
+
+enum spriteID {sprAnt = 1, sprMap};
+enum imageID {iAnt = 1, iFloor, iWall};
+enum idPriorities {priMap = 1, priAnt };
+// Sets mapsize and width and height of map sprites
+const int mapW = 640, mapH = 640, sprW = 64, sprH = 64;
+const int tileCount = (MAP_WIDTH - 1) * MAP_HEIGHT;
+
+// Limit screenscrolling by setting min and max map edges.
+const int minLeftEdge = 0, maxLeftEdge = ((MAP_WIDTH - 1) * sprW) - mapW,
+		  minTopEdge = 0, maxTopEdge = (MAP_HEIGHT * sprH) - mapH;
+
+
+/** Limit the player movement: minimum is the left edge and top of the map area
+*** Starts at 32 so half the sprite can't walk out of the map.
+*** maximum is the right and bottom edge of the map area
+*** minus one half sprite so half the sprite doesn't walk out of the map.
+**/
+
+const int minPlayerXPos = 32, maxPlayerXPos = 9.5 * sprW;
+const int minPlayerYPos = 32, maxPlayerYPos = 9.5 * sprH;
+
+// sets player movement speed in pixels per second
+const float moveSpeed = 5;
 
 // level map array
-const char mapArray[MAP_WIDTH][MAP_HEIGHT] ={	"WWWWWWWWWX",
-												"WFFFFWFWFF",
-												"WFWWFFFWFW",
-												"WFFFWWFWFW",
-												"WWWFWWFWFW",
-												"WWWFFWFWFW",
-												"WWWWFWFWFW",
-												"WWFFFWFFFW",
-												"FFFWWWWWWW",
-												"EWWWWWWWWW" };
+const char mapArray[MAP_HEIGHT][MAP_WIDTH] ={	"EWWWWWWWWWWWWWWWWWWW",
+												"FFFFFFFFFFWFFFFFFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFFFFFFFFFFFFFFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFW",
+												"WFFFWWFFFWWFFFWWFFFF",
+												"WWWWWWWWWWWWWWWWWWWX"  };
--- Source/Player.h
+++ Source/Player.h
@@ -1,7 +1,31 @@
+#pragma once
 
+
+class PlayerAnt
+{
+public:
+	// The constructor and destructor will create/delete
+	// the player image and player sprite.
+	 PlayerAnt(float antStartX, float antStartY);
+	~PlayerAnt();
 
 	
+	// Set the (starting) position of the player.
+	void SetAntStart();
+	void SetAntPos(float X, float Y);
 	void PlayerMove();
-	void CreateAntSprite();
+	
+	// This sets the new coordinates of the player, according to movement keys pressed.
+	// It returns the current X position of the player.
+	// The input parameter is the elapsed time and the current left edge of the screen.
+
+	//int updateAnt(float TimeDiff, int LeftEdge);
+
+private:
+	float antPosX, antPosY;
+	float antLastPosX, antLastPosY, antStartPosX, antStartPosY;
+
+}; 
+	
 	
 	
